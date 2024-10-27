1c7b62245d9ed107b4b984ea4f63053316e89d74
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-10-27 07:50:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
+    e.preventDefault();
+
+    let r = parseFloat(document.getElementById('radius').value)
+
+    if(isNaN(r) || r < 0){
+        alert("Please enter positive value")
+        return;
+    }
+
+    let vol = 4/3 * Math.PI * r * r * r;
+
+    document.getElementById('volume').value = vol.toFixed(4)
+
+
   
 } 
 
